140e23cf0ecf2a0b530ff45fdb3bf260c9dacead
Atsushi Togo <atz.togo@gmail.com>
2013-02-07 14:54:35 +0900

    Update setup.py for packaging

--- setup.py
+++ setup.py
@@ -4,30 +4,32 @@ import numpy
 include_dirs_numpy = [numpy.get_include()]
 
 extension = Extension('phonopy._phonopy',
-                      include_dirs = ['c'] + include_dirs_numpy,
-                      sources = ['c/_phonopy.c'])
+                      include_dirs=['c'] + include_dirs_numpy,
+                      sources=['c/_phonopy.c'])
 
-extension_spglib = Extension('phonopy._spglib',
-                             include_dirs = ['c/spglib'] + include_dirs_numpy,
-                             # extra_compile_args=['-fopenmp'],
-                             # extra_link_args=['-lgomp'],
-                             sources = ['c/_spglib.c',
-                                        'c/spglib/refinement.c',
-                                        'c/spglib/cell.c',
-                                        'c/spglib/debug.c',
-                                        'c/spglib/hall_symbol.c',
-                                        'c/spglib/kpoint.c',
-                                        'c/spglib/lattice.c',
-                                        'c/spglib/mathfunc.c',
-                                        'c/spglib/pointgroup.c',
-                                        'c/spglib/primitive.c',
-                                        'c/spglib/spacegroup.c',
-                                        'c/spglib/spg_database.c',
-                                        'c/spglib/spglib.c',
-                                        'c/spglib/spin.c',
-                                        'c/spglib/site_symmetry.c',
-                                        'c/spglib/sitesym_database.c',
-                                        'c/spglib/symmetry.c'] )
+extension_spglib = Extension(
+    'phonopy._spglib',
+    include_dirs=['c/spglib_include'] + include_dirs_numpy,
+    # extra_compile_args=['-fopenmp'],
+    # extra_link_args=['-lgomp'],
+    sources=['c/_spglib.c',
+             'c/spglib/cell.c',
+             'c/spglib/debug.c',
+             'c/spglib/hall_symbol.c',
+             'c/spglib/kpoint.c',
+             'c/spglib/lattice.c',
+             'c/spglib/mathfunc.c',
+             'c/spglib/pointgroup.c',
+             'c/spglib/primitive.c',
+             'c/spglib/refinement.c',
+             'c/spglib/sitesym_database.c',
+             'c/spglib/site_symmetry.c',
+             'c/spglib/spacegroup.c',
+             'c/spglib/spg_database.c',
+             'c/spglib/spglib.c',
+             'c/spglib/spin.c',
+             'c/spglib/symmetry.c'])
+
 
 setup(name='phonopy',
       version='1.6.3',
