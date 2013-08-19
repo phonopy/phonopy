2c57cae321265fed6129eaf133a4338b9dae0bc6
Atsushi Togo <atz.togo@gmail.com>
2013-08-19 17:43:36 +0900

    Frequency shift code implementation commences

---
+++ anharmonic/phonon4/frequency_shift.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class FrequenecyShift:
+    def __init__(self):
+        pass
+
+class ReadToReciprocal:
+    def __init__(self):
+        pass
+
+class ReciprocalToNormal:
+    def __init__(self):
+        pass
