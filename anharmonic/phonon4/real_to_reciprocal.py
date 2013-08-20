e365593ae0aea9480611c45604593450bc699403
Atsushi Togo <atz.togo@gmail.com>
2013-08-20 17:44:16 +0900

    Frequency shift code implementation

---
+++ anharmonic/phonon4/real_to_reciprocal.py
@@ -0,0 +1,72 @@
+import numpy as np
+from phonopy.harmonic.dynamical_matrix import get_smallest_vectors
+
+class RealToReciprocal:
+    def __init__(self,
+                 fc4,
+                 supercell,
+                 primitive,
+                 mesh,
+                 symprec=1e-5):
+        self._fc4 = fc4
+        self._supercell = supercell
+        self._primitive = primitive
+        self._mesh = mesh
+        self._symprec = symprec
+
+        num_satom = supercell.get_number_of_atoms()
+        self._p2s_map = np.intc(primitive.get_primitive_to_supercell_map())
+        self._s2p_map = np.intc(primitive.get_supercell_to_primitive_map())
+        (self._smallest_vectors,
+         self._multiplicity) = get_smallest_vectors(supercell,
+                                                    primitive,
+                                                    symprec)
+        self._quartet = None
+        self._fc4_reciprocal = None
+
+    def run(self, quartet):
+        self._quartet = quartet
+        num_patom = self._primitive.get_number_of_atoms()
+        self._fc4_reciprocal = np.zeros(
+            (num_patom,) * 4 + (3,) * 4, dtype='complex128')
+        self._real_to_reciprocal()
+
+    def get_fc4_reciprocal(self):
+        return self._fc4_reciprocal
+
+    def _real_to_reciprocal(self):
+        num_patom = self._primitive.get_number_of_atoms()
+        for (i, j, k, l) in list(np.ndindex(
+                num_patom, num_patom, num_patom, num_patom)):
+            self._fc4_reciprocal[
+                i, j, k, l] = self._real_to_reciprocal_elements((i, j, k, l))
+
+    def _real_to_reciprocal_elements(self, patom_indices):
+        num_satom = self._supercell.get_number_of_atoms()
+        pi = patom_indices
+        i = self._p2s_map[pi[0]]
+        fc4_reciprocal = np.zeros((3, 3, 3, 3), dtype='complex128')
+        for j in range(num_satom):
+            if self._s2p_map[j] != self._p2s_map[pi[1]]:
+                continue
+            for k in range(num_satom):
+                if self._s2p_map[k] != self._p2s_map[pi[2]]:
+                    continue
+                for l in range(num_satom):
+                    if self._s2p_map[l] != self._p2s_map[pi[3]]:
+                        continue
+                    phase = self._get_phase((j, k, l), pi[0])
+                    fc4_reciprocal += self._fc4[i, j, k, l] * phase
+        return fc4_reciprocal
+
+    def _get_phase(self, satom_indices, patom0_index):
+        si = satom_indices
+        p0 = patom0_index
+        phase = 1+0j
+        for i in (0, 1, 2):
+            vs = self._smallest_vectors[si[i], p0,
+                                        :self._multiplicity[si[i], p0]]
+            phase *= (np.exp(2j * np.pi * np.dot(
+                        vs, self._quartet[i + 1].astype('double') /
+                        self._mesh)).sum() / self._multiplicity[si[i], p0])
+        return phase
