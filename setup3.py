622999275cb42f3fb6c201819c658290046adeba
Atsushi Togo <atz.togo@gmail.com>
2013-09-03 15:57:18 +0900

    Minor change & set version as 0.5.3

--- setup3.py
+++ setup3.py
@@ -65,7 +65,7 @@ extension_forcefit = Extension(
              'c/harmonic/lapack_wrapper.c'])
 
 setup(name='phono3py',
-      version='0.5.1',
+      version='0.5.3',
       description='This is the phono3py module.',
       author='Atsushi Togo',
       author_email='atz.togo@gmail.com',
