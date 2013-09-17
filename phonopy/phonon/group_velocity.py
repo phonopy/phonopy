c070076abad01dc6cfe871072ce26ac7a945b1d9
Atsushi Togo <atz.togo@gmail.com>
2013-09-17 16:52:02 +0900

    Symmetrizing group velocity using stabilizer of q-point

--- phonopy/phonon/group_velocity.py
+++ phonopy/phonon/group_velocity.py
@@ -35,10 +35,12 @@
 import numpy as np
 from phonopy.units import VaspToTHz
 from phonopy.harmonic.derivative_dynmat import DerivativeOfDynamicalMatrix
+from phonopy.harmonic.force_constants import similarity_transformation
 
 def get_group_velocity(q, # q-point
                        dynamical_matrix,
                        q_length=None, # finite distance in q
+                       symmetry=None,
                        frequency_factor_to_THz=VaspToTHz):
     """
     If frequencies and eigenvectors are supplied they are used
@@ -51,9 +53,10 @@ def get_group_velocity(q, # q-point
     """
 
     gv = GroupVelocity(dynamical_matrix,
-                       [q],
                        q_length=q_length,
+                       symmetry=symmetry,
                        frequency_factor_to_THz=frequency_factor_to_THz)
+    gv.set_q_points([q])
     return gv.get_group_velocity()[0]
 
 def degenerate_sets(freqs, cutoff=1e-4):
@@ -98,8 +101,8 @@ class GroupVelocity:
     
     def __init__(self,
                  dynamical_matrix,
-                 q_points=None,
                  q_length=None,
+                 symmetry=None,
                  frequency_factor_to_THz=VaspToTHz):
         """
         q_points is a list of sets of q-point and q-direction:
@@ -110,12 +113,13 @@ class GroupVelocity:
         self._dynmat = dynamical_matrix
         primitive = dynamical_matrix.get_primitive()
         self._reciprocal_lattice_inv = primitive.get_cell()
-        self._q_points = q_points
+        self._reciprocal_lattice = np.linalg.inv(self._reciprocal_lattice_inv)
         self._q_length = q_length
         if q_length is None:
             self._ddm = DerivativeOfDynamicalMatrix(dynamical_matrix)
         else:
             self._ddm = None
+        self._symmetry = symmetry
         self._factor = frequency_factor_to_THz
 
         self._directions = np.array([[1, 2, 3],
@@ -124,9 +128,8 @@ class GroupVelocity:
                                      [0, 0, 1]], dtype='double')
         self._directions[0] /= np.linalg.norm(self._directions[0])
 
+        self._q_points = None
         self._group_velocity = None
-        if self._q_points is not None:
-            self._set_group_velocity()
 
     def set_q_points(self, q_points):
         self._q_points = q_points
@@ -139,11 +142,8 @@ class GroupVelocity:
         return self._group_velocity
 
     def _set_group_velocity(self):
-        v_g = []
-        for q in self._q_points:
-            dD_at_q = self._set_group_velocity_at_q(q)
-            v_g.append(dD_at_q)
-        self._group_velocity = np.array(v_g)
+        gv = [self._set_group_velocity_at_q(q) for q in self._q_points]
+        self._group_velocity = np.array(gv)
 
     def _set_group_velocity_at_q(self, q):
         self._dynmat.set_dynamical_matrix(q)
@@ -151,7 +151,6 @@ class GroupVelocity:
         eigvals, eigvecs = np.linalg.eigh(dm)
         eigvals = eigvals.real
         freqs = np.sqrt(abs(eigvals)) * np.sign(eigvals) * self._factor
-    
         gv = np.zeros((len(freqs), 3), dtype='double')
         deg_sets = degenerate_sets(freqs)
 
@@ -164,8 +163,23 @@ class GroupVelocity:
         for i in range(3):
             gv[:, i] *= self._factor ** 2 / freqs / 2
 
-        return gv
+        return self._symmetrize_group_velocity(gv, q)
 
+    def _symmetrize_group_velocity(self, gv, q):
+        rotations = []
+        for r in self._symmetry.get_reciprocal_operations():
+            diff = q - np.dot(r, q)
+            diff -= np.rint(diff)
+            if (np.abs(diff) < self._symmetry.get_symmetry_tolerance()).all():
+                rotations.append(r)
+
+        gv_sym = np.zeros_like(gv)
+        for r in rotations:
+            r_cart = similarity_transformation(self._reciprocal_lattice, r)
+            gv_sym += np.dot(r_cart, gv.T).T
+
+        return gv_sym / len(rotations)
+    
     def _get_dD(self, q):
         if self._q_length is None:
             return self._get_dD_analytical(q)
