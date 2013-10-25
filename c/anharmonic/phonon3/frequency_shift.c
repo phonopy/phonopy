317db4f7a4378e6bdfa4e423b3fe50a07a5f4737
Atsushi Togo <atz.togo@gmail.com>
2013-10-25 23:37:33 +0900

    Preparing implementation of instantaneous phonon

---
+++ c/anharmonic/phonon3/frequency_shift.c
@@ -0,0 +1,7 @@
+#include <lapacke.h>
+#include <stdlib.h>
+#include "phonoc_array.h"
+#include "phonoc_math.h"
+#include "phonoc_utils.h"
+#include "phonon3_h/frequency_shift.h"
+#include "phonon3_h/real_to_reciprocal.h"
---
+++ c/anharmonic_h/phonon3_h/frequency_shift.h
@@ -0,0 +1,7 @@
+#ifndef __frequency_shift_H__
+#define __frequency_shift_H__
+
+#include <lapacke.h>
+#include "phonoc_array.h"
+
+#endif
