dee926a4b1476cb83d7b0ea839a9171662c3e82c
Atsushi Togo <atz.togo@gmail.com>
2013-11-27 18:19:11 +0900

    Rewrite creation of displacement matrix

--- anharmonic/force_fit/fc2.py
+++ anharmonic/force_fit/fc2.py
@@ -38,14 +38,21 @@ class FC2Fit:
                 sets_of_forces.append(dataset_1st['forces'])
 
             site_symmetry = self._symmetry.get_site_symmetry(first_atom_num)
+            positions = (self._positions.copy() -
+                         self._positions[first_atom_num])
+            rot_map_syms = get_positions_sent_by_rot_inv(positions,
+                                                         site_symmetry,
+                                                         self._symprec)
             site_sym_cart = [similarity_transformation(self._lattice, sym)
                              for sym in site_symmetry]
-            rot_disps = get_rotated_displacement(disps, site_sym_cart)
+            # rot_disps = get_rotated_displacement(disps, site_sym_cart)
+            rot_disps = self._create_displacement_matrix(disps,
+                                                         site_sym_cart)
             rot_forces = self._create_force_matrix(sets_of_forces,
-                                                   site_symmetry,
                                                    site_sym_cart,
-                                                   first_atom_num)
-            self._fc2[first_atom_num, :] = self._solve(rot_disps, rot_forces)
+                                                   rot_map_syms)
+            fc = self._solve(rot_disps, rot_forces)
+            self._fc2[first_atom_num, :] = fc[:, 1:, :]
 
         rotations = self._symmetry.get_symmetry_operations()['rotations']
         trans = self._symmetry.get_symmetry_operations()['translations']
@@ -60,24 +67,34 @@ class FC2Fit:
 
     def _create_force_matrix(self,
                              sets_of_forces,
-                             site_symmetry,
                              site_sym_cart,
-                             disp_atom_num):
-        positions = (self._positions.copy() -
-                     self._positions[disp_atom_num])
-        rot_map_syms = get_positions_sent_by_rot_inv(positions,
-                                                     site_symmetry,
-                                                     self._symprec)
+                             rot_map_syms):
         force_matrix = []
         for i in range(self._num_atom):
             for forces in sets_of_forces:
-                force_matrix.append(
-                    get_rotated_forces(forces[rot_map_syms[:, i]],
-                                       site_sym_cart))
+                for f, ssym_c in zip(
+                    forces[rot_map_syms[:, i]], site_sym_cart):
+                    force_matrix.append(np.dot(ssym_c, f))
         return np.reshape(force_matrix, (self._num_atom, -1, 3))
+
+    def _create_displacement_matrix(self,
+                                    disps,
+                                    site_sym_cart):
+        rot_disps = []
+        for ssym_c in site_sym_cart:
+            for u in disps:
+                Su = np.dot(ssym_c, u)
+                rot_disps.append(Su)
+    
+        ones = np.ones(len(rot_disps)).reshape((-1, 1))
+
+        return np.hstack((ones, rot_disps))
         
     def _solve(self, rot_disps, rot_forces):
         inv_disps = np.linalg.pinv(rot_disps)
-        return [-np.dot(inv_disps, f) for f in rot_forces]
+        print inv_disps.shape
+        print rot_forces.shape
+        return np.array([-np.dot(inv_disps, f) for f in rot_forces],
+                        dtype='double')
         
             
