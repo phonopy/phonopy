f201a6fad4aeebee1ef319d605f175e66e8bf79a
Atsushi Togo <atz.togo@gmail.com>
2013-12-23 22:34:39 +0900

    Phonopy version 1.8.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ extension_spglib = Extension(
 
 
 setup(name='phonopy',
-      version='1.7.6',
+      version='1.8.0',
       description='This is the phonopy module.',
       author='Atsushi Togo',
       author_email='atz.togo@gmail.com',
