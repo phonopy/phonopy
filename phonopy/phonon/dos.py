d759e1ae8aa6002c6cc5832adee3557499af4439
Atsushi Togo <atz.togo@gmail.com>
2014-01-08 19:30:22 +0900

    Separate out ploting and writing functions out of class

--- phonopy/phonon/dos.py
+++ phonopy/phonon/dos.py
@@ -34,6 +34,82 @@
 
 import numpy as np
 
+
+def write_total_dos(frequency_points,
+                    total_dos,
+                    sigma=None):
+    fp = open('total_dos.dat', 'w')
+    if sigma is not None:
+        fp.write("# Sigma = %f\n" % sigma)
+
+    for freq, dos in zip(frequency_points, total_dos):
+        fp.write("%20.10f%20.10f\n" % (freq, dos))
+
+def write_partial_dos(frequency_points,
+                      partial_dos,
+                      sigma=None):
+    fp = open('partial_dos.dat', 'w')
+    if sigma is not None:
+        fp.write("# Sigma = %f\n" % sigma)
+        
+    for freq, pdos in zip(frequency_points, partial_dos.T):
+        fp.write("%20.10f" % freq)
+        fp.write(("%20.10f" * len(pdos)) % tuple(pdos))
+        fp.write("\n")
+
+
+def plot_total_dos(frequency_points,
+                   total_dos,
+                   freq_Debye=None,
+                   Debye_fit_coef=None):
+    import matplotlib.pyplot as plt
+    plt.plot(frequency_points, total_dos, 'r-')
+
+    if freq_Debye is not None:
+        freq_pitch = frequency_points[1] - frequency_points[0]
+        num_points = int(freq_Debye / freq_pitch)
+        freqs = np.linspace(0, freq_Debye, num_points + 1)
+        plt.plot(np.append(freqs, freq_Debye),
+                 np.append(Debye_fit_coef * freqs**2, 0), 'b-')
+    plt.grid(True)
+    plt.xlim(min(frequency_points), max(frequency_points))
+    plt.xlabel('Frequency')
+    plt.ylabel('Density of states')
+    
+    return plt
+
+def plot_partial_tos(frequency_points,
+                     partial_dos,
+                     indices=None,
+                     legend=None):
+    import matplotlib.pyplot as plt
+    plt.grid(True)
+    plt.xlim(min(frequency_points), max(frequency_points))
+    plt.xlabel('Frequency')
+    plt.ylabel('Partial density of states')
+    plots = []
+
+    num_atom = len(partial_dos)
+
+    if indices == None:
+        indices = []
+        for i in range(num_atom):
+            indices.append([i])
+
+    for set_for_sum in indices:
+        pdos_sum = np.zeros(frequency_points.shape, dtype='double')
+        for i in set_for_sum:
+            if i > num_atom - 1 or i < 0:
+                print "Your specified atom number is out of range."
+                raise ValueError
+            pdos_sum += partial_dos[i]
+        plots.append(plt.plot(frequency_points, pdos_sum))
+
+    if not legend==None:
+        plt.legend(legend)
+
+    return plt
+    
 class NormalDistribution:
     def __init__(self, sigma):
         self._sigma = sigma
@@ -104,9 +180,10 @@ class Dos:
 class TotalDos(Dos):
     def __init__(self, frequencies, weights, sigma=None):
         Dos.__init__(self, frequencies, weights, sigma)
-        self._freq_Debye = None
-        self._freqs = None
+        self._frequency_points = None
         self._dos = None
+        self._freq_Debye = None
+        self._Debye_fit_coef = None
 
     def calculate(self):
         freq = self._freq_min
@@ -116,14 +193,14 @@ class TotalDos(Dos):
             freq += self._freq_pitch
 
         dos = np.array(dos)
-        self._freqs = dos[:,0]
+        self._frequency_points = dos[:,0]
         self._dos = dos[:,1]
 
     def get_dos(self):
         """
         Return freqs and total dos
         """
-        return self._freqs, self._dos
+        return self._frequency_points, self._dos
 
     def get_Debye_frequency(self):
         return self._freq_Debye
@@ -138,41 +215,31 @@ class TotalDos(Dos):
         def Debye_dos(freq, a):
             return a * freq**2
 
-        freqs_min = self._freqs.min()
-        freqs_max = self._freqs.max()
+        freqs_min = self._frequency_points.min()
+        freqs_max = self._frequency_points.max()
         
         if freq_max_fit is None:
-            N_fit = len(self._freqs) / 4 # Hard coded
+            N_fit = len(self._frequency_points) / 4 # Hard coded
         else:
             N_fit = int(freqs_max_fit / (freqs_max - freqs_min) *
-                        len(self._freqs.size))
+                        len(self._frequency_points.size))
         popt, pcov = curve_fit(Debye_dos,
-                               self._freqs[0:N_fit],
+                               self._frequency_points[0:N_fit],
                                self._dos[0:N_fit])
         a2 = popt[0]
         self._freq_Debye = (3 * 3 * num_atoms / a2)**(1.0 / 3)
         self._Debye_fit_coef = a2
 
     def plot_dos(self):
-        import matplotlib.pyplot as plt
-        plt.plot(self._freqs, self._dos, 'r-')
-        if self._freq_Debye:
-            num_points = int(self._freq_Debye / self._freq_pitch)
-            freqs = np.linspace(0, self._freq_Debye, num_points + 1)
-            plt.plot(np.append(freqs, self._freq_Debye),
-                     np.append(self._Debye_fit_coef * freqs**2, 0), 'b-')
-        plt.grid(True)
-        plt.xlabel('Frequency')
-        plt.ylabel('Density of states')
-        
-        return plt
-    
+        return plot_total_dos(self._frequency_points,
+                              self._dos,
+                              freq_Debye=self._freq_Debye,
+                              Debye_fit_coef=self._Debye_fit_coef)
+
     def write(self):
-        file = open('total_dos.dat', 'w')
-        file.write("# Sigma = %f\n" % self._sigma)
-        for freq, dos in zip(self._freqs, self._dos):
-            file.write("%20.10f%20.10f" % (freq, dos))
-            file.write("\n")
+        write_total_dos(self._frequency_points,
+                        self._dos,
+                        sigma=self._sigma)
 
     def _get_density_of_states_at_freq(self, freq):
         return np.sum(np.dot(
@@ -208,7 +275,7 @@ class PartialDos(Dos):
             self._eigvecs2 += np.abs(self._eigenvectors[:, i_y, :]) ** 2
             self._eigvecs2 += np.abs(self._eigenvectors[:, i_z, :]) ** 2
         self._partial_dos = None
-        self._freqs = None
+        self._frequency_points = None
 
     def calculate(self):
         freq = self._freq_min
@@ -223,58 +290,29 @@ class PartialDos(Dos):
             pdos.append(pdos_at_freq)
 
         self._partial_dos = np.array(pdos).T
-        self._freqs = np.array(freqs)
+        self._frequency_points = np.array(freqs)
 
     def get_partial_dos(self):
         """
-        freqs: Sampling frequencies
-        partial_dos:
-          [[atom1-band1, atom1-band2, ... ],
-           [atom2-band1, atom2-band2, ... ],
-           ... ]
+        frequency_points: Sampling frequencies
+        partial_dos: [atom_index, frequency_points_index]
         """
-        return self._freqs, self._partial_dos
+        return self._frequency_points, self._partial_dos
 
     def plot_pdos(self, indices=None, legend=None):
-        import matplotlib.pyplot as plt
-        plt.grid(True)
-        plt.xlim(self._freq_min, self._freq_max)
-        plt.xlabel('Frequency')
-        plt.ylabel('Partial density of states')
-        plots = []
-
-        num_atom = self._frequencies.shape[1] / 3
-
-        if indices == None:
-            indices = []
-            for i in range(num_atom):
-                indices.append([i])
-
-        for set_for_sum in indices:
-            pdos_sum = np.zeros(self._freqs.shape, dtype=float)
-            for i in set_for_sum:
-                if i > num_atom - 1 or i < 0:
-                    print "Your specified atom number is out of range."
-                    raise ValueError
-                pdos_sum += self._partial_dos[i]
-            plots.append(plt.plot(self._freqs, pdos_sum))
-
-        if not legend==None:
-            plt.legend(legend)
-
-        return plt
-
-
+        return plot_partial_tos(self._frequency_points,
+                                self._partial_dos,
+                                indices=indices,
+                                legend=legend)
+    
     def write(self):
-        file = open('partial_dos.dat', 'w')
-        file.write("# Sigma = %f\n" % self._sigma)
-        for freq, pdos in zip(self._freqs, self._partial_dos.transpose()):
-            file.write("%20.10f" % freq)
-            file.write(("%20.10f" * len(pdos)) % tuple(pdos))
-            file.write("\n")
+        write_partial_dos(self._frequency_points,
+                          self._partial_dos,
+                          sigma=self._sigma)
 
     def _get_partial_dos_at_freq(self, amplitudes, weights):
         num_band = self._frequencies.shape[1]
         pdos = [(np.dot(weights, self._eigvecs2[:, i, :] * amplitudes)).sum()
                 for i in range(num_band / 3)]
         return pdos
+
