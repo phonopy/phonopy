93a051e1234a4d0165986ed755115e0641efec5e
Atsushi Togo <atz.togo@gmail.com>
2014-01-11 18:18:09 +0900

    Separate phono4py and force_fit from setup3.py to setup4.py

--- setup3.py
+++ setup3.py
@@ -7,8 +7,9 @@ include_dirs_lapacke = ['../lapacke/include']
 extension = Extension(
     'anharmonic._phono3py',
     include_dirs=(['c/harmonic_h',
-                   'c/anharmonic_h'] +
-                  include_dirs_numpy +
+                   'c/anharmonic_h',
+                   'c/spglib_h'] +
+                   include_dirs_numpy +
                   include_dirs_lapacke),
     extra_compile_args=['-fopenmp'],
     extra_link_args=['-lgomp',
@@ -27,42 +28,10 @@ extension = Extension(
              'c/anharmonic/phonon3/real_to_reciprocal.c',
              'c/anharmonic/phonon3/reciprocal_to_normal.c',
              'c/anharmonic/phonon3/imag_self_energy.c',
-             'c/anharmonic/other/isotope.c'])
-
-extension_phono4py = Extension(
-    'anharmonic._phono4py',
-    include_dirs=(['c/harmonic_h',
-                   'c/anharmonic_h'] +
-                  include_dirs_numpy +
-                  include_dirs_lapacke),
-    extra_compile_args=['-fopenmp'],
-    extra_link_args=['-lgomp',
-                     '../lapacke/liblapacke.a',
-                     '-llapack',
-                     '-lblas'],
-    sources=['c/_phono4py.c',
-             'c/harmonic/dynmat.c',
-             'c/harmonic/lapack_wrapper.c',
-             'c/anharmonic/phonoc_array.c',
-             'c/anharmonic/phonoc_math.c',
-             'c/anharmonic/phonoc_utils.c',
-             'c/anharmonic/phonon3/fc3.c',
-             'c/anharmonic/phonon4/fc4.c',
-             'c/anharmonic/phonon4/real_to_reciprocal.c',
-             'c/anharmonic/phonon4/frequency_shift.c'])
-
-extension_forcefit = Extension(
-    'anharmonic._forcefit',
-    include_dirs=(['c/harmonic_h'] +
-                  include_dirs_numpy +
-                  include_dirs_lapacke),
-    extra_compile_args=['-fopenmp'],
-    extra_link_args=['-lgomp',
-                     '../lapacke/liblapacke.a',
-                     '-llapack',
-                     '-lblas'],
-    sources=['c/_forcefit.c',
-             'c/harmonic/lapack_wrapper.c'])
+             'c/anharmonic/other/isotope.c',
+             'c/spglib/debug.c',
+             'c/spglib/mathfunc.c',
+             'c/spglib/tetrahedron_method.c'])
 
 setup(name='phono3py',
       version='0.8.0',
@@ -71,13 +40,7 @@ setup(name='phono3py',
       author_email='atz.togo@gmail.com',
       url='http://phonopy.sourceforge.net/',
       packages=['anharmonic',
-                'anharmonic.force_fit',
                 'anharmonic.other',
-                'anharmonic.phonon3',
-                'anharmonic.phonon4'],
-      scripts=['scripts/force-fit',
-               'scripts/phono3py',
-               'scripts/phono4py'],
-      ext_modules=[extension,
-                   extension_phono4py,
-                   extension_forcefit])
+                'anharmonic.phonon3'],
+      scripts=['scripts/phono3py'],
+      ext_modules=[extension])
---
+++ setup4.py
@@ -0,0 +1,54 @@
+from distutils.core import setup, Extension
+#from setuptools import setup, Extension
+import numpy
+include_dirs_numpy = [numpy.get_include()]
+include_dirs_lapacke = ['../lapacke/include']
+
+extension_phono4py = Extension(
+    'anharmonic._phono4py',
+    include_dirs=(['c/harmonic_h',
+                   'c/anharmonic_h'] +
+                  include_dirs_numpy +
+                  include_dirs_lapacke),
+    extra_compile_args=['-fopenmp'],
+    extra_link_args=['-lgomp',
+                     '../lapacke/liblapacke.a',
+                     '-llapack',
+                     '-lblas'],
+    sources=['c/_phono4py.c',
+             'c/harmonic/dynmat.c',
+             'c/harmonic/lapack_wrapper.c',
+             'c/anharmonic/phonoc_array.c',
+             'c/anharmonic/phonoc_math.c',
+             'c/anharmonic/phonoc_utils.c',
+             'c/anharmonic/phonon3/fc3.c',
+             'c/anharmonic/phonon4/fc4.c',
+             'c/anharmonic/phonon4/real_to_reciprocal.c',
+             'c/anharmonic/phonon4/frequency_shift.c'])
+
+extension_forcefit = Extension(
+    'anharmonic._forcefit',
+    include_dirs=(['c/harmonic_h'] +
+                  include_dirs_numpy +
+                  include_dirs_lapacke),
+    extra_compile_args=['-fopenmp'],
+    extra_link_args=['-lgomp',
+                     '../lapacke/liblapacke.a',
+                     '-llapack',
+                     '-lblas'],
+    sources=['c/_forcefit.c',
+             'c/harmonic/lapack_wrapper.c'])
+
+setup(name='phono4py',
+      version='0.8.0',
+      description='This is the phono3py module.',
+      author='Atsushi Togo',
+      author_email='atz.togo@gmail.com',
+      url='http://phonopy.sourceforge.net/',
+      packages=['anharmonic',
+                'anharmonic.force_fit',
+                'anharmonic.phonon4'],
+      scripts=['scripts/force-fit',
+               'scripts/phono4py'],
+      ext_modules=[extension_phono4py,
+                   extension_forcefit])
