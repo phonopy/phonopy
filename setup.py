e27c33ab40e7096fb897d477c60d988f3eb9a9d4
Atsushi Togo <atz.togo@gmail.com>
2014-04-05 16:54:00 +0900

    Minor version 1.8.4.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extension_spglib = Extension(
 
 
 setup(name='phonopy',
-      version='1.8.4',
+      version='1.8.4.1',
       description='This is the phonopy module.',
       author='Atsushi Togo',
       author_email='atz.togo@gmail.com',
