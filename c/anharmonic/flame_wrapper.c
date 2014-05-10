af9938eff297c534aeb08c56195c46d1358f80f5
Atsushi Togo <atz.togo@gmail.com>
2014-05-10 16:33:05 +0900

    Libflame Hevd version

--- c/anharmonic/flame_wrapper.c
+++ c/anharmonic/flame_wrapper.c
@@ -12,9 +12,7 @@ int flame_Hevd(double *matrix,
   FLA_Obj_attach_buffer(matrix, 0, 0, &A);
   FLA_Obj_create_without_buffer(FLA_DOUBLE, 1, size, &l);
   FLA_Obj_attach_buffer(eigvals, 0, 0, &l);
-  printf("start Hevd\n");
   FLA_Hevd(FLA_EVD_WITH_VECTORS, FLA_LOWER_TRIANGULAR, A, l);
-  printf("end Hevd\n");
   FLA_Obj_free_without_buffer(&A);
   FLA_Obj_free_without_buffer(&l);
   FLA_Finalize();
