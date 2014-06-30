f8305a116ec1a3b250f6abc1d62fdf23292e0ae5
Atsushi Togo <atz.togo@gmail.com>
2014-06-30 22:13:23 +0900

    Fix bug in finding equivalent atoms tantatively.

--- phonopy/structure/symmetry.py
+++ phonopy/structure/symmetry.py
@@ -152,7 +152,9 @@ class Symmetry:
         self._international_table = "%s (%d)" % (self._dataset['international'],
                                                  self._dataset['number'])
         self._wyckoff_letters = self._dataset['wyckoffs']
-        self._map_atoms = self._dataset['equivalent_atoms']
+        
+        # self._map_atoms = self._dataset['equivalent_atoms']
+        self._set_map_atoms()
 
     def _set_symmetry_operations(self):
         self._symmetry_operations = spg.get_symmetry(self._cell,
