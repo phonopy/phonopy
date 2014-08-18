92870086c19e34d129aab283fc42005adab8951b
Atsushi Togo <atz.togo@gmail.com>
2014-08-18 17:27:16 +0900

    Fix user interface problem

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extension_spglib = Extension(
 
 
 setup(name='phonopy',
-      version='1.8.5',
+      version='1.8.5.1',
       description='This is the phonopy module.',
       author='Atsushi Togo',
       author_email='atz.togo@gmail.com',
