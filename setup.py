f74619182f7830829c3c917a3bc111aa02ef660d
Atsushi Togo <atz.togo@gmail.com>
2014-09-17 16:16:27 +0900

    Set version as 1.9.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extension_spglib = Extension(
 
 
 setup(name='phonopy',
-      version='1.9.0.1',
+      version='1.9.1',
       description='This is the phonopy module.',
       author='Atsushi Togo',
       author_email='atz.togo@gmail.com',
