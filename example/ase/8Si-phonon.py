f2985904136601e6b0ef0bdcaf9065778bdbeba9
Atsushi Togo <atz.togo@gmail.com>
2014-10-01 15:24:31 +0900

    Update gpaw example

--- example/ase/8Si-phonon.py
+++ example/ase/8Si-phonon.py
@@ -1,6 +1,5 @@
-from gpaw import GPAW
+from gpaw import GPAW, PW
 from ase import *
-from ase.calculators import numeric_force
 from phonopy import Phonopy
 from phonopy.structure.atoms import Atoms as PhonopyAtoms
 import numpy as np
@@ -18,49 +17,43 @@ bulk = PhonopyAtoms(symbols=['Si']*8,
                                       (0.75, 0.75, 0.25)] )
 bulk.set_cell(np.diag((a, a, a)))
 
-n = 24
-calc = GPAW(gpts=(n, n, n),
-            nbands=8*3,
-            width=0.01,
-            kpts=(2, 2, 2),
-            convergence={'eigenstates': 1e-9}
-            )
+calc = GPAW(mode=PW(300),
+            kpts={'size': (4, 4, 4)},
+            symmetry={'symmorphic': False})
 
-# Phonopy pre-process
-print "------"
-print "Phonon"
-print "------"
-# 1st arg. is the input unit cell.
-# 2nd arg. is the supercell lattice relative to the unit cell.
-# 'distance' is the distance of displacements.
-# Default symmetry tolerance is 1e-5 in fractional coordinates.
-phonon = Phonopy(bulk, [[1,0,0],[0,1,0],[0,0,1]], distance=0.01)
-phonon.print_displacements()
+phonon = Phonopy(bulk,
+                 [[1,0,0],[0,1,0],[0,0,1]],
+                 primitive_matrix=[[0, 0.5, 0.5],
+                                   [0.5, 0, 0.5],
+                                   [0.5, 0.5, 0]],
+                 distance=0.01)
+print "[Phonopy] Atomic displacements:"
+disps = phonon.get_displacements()
+for d in disps:
+    print "[Phonopy]", d[0], d[1:]
 supercells = phonon.get_supercells_with_displacements()
 
 # Force calculations by calculator
 set_of_forces = []
 for scell in supercells:
-    cell = Atoms( symbols=scell.get_chemical_symbols(),
-                  scaled_positions=scell.get_scaled_positions(),
-                  cell=scell.get_cell(),
-                  pbc=True )
+    cell = Atoms(symbols=scell.get_chemical_symbols(),
+                 scaled_positions=scell.get_scaled_positions(),
+                 cell=scell.get_cell(),
+                 pbc=True)
     cell.set_calculator(calc)
     forces = cell.get_forces()
     drift_force = forces.sum(axis=0)
-    print "        ---------------------------------"
-    print "     ", "%11.5f"*3 % tuple(drift_force)
+    print "[Phonopy] Drift force:", "%11.5f"*3 % tuple(drift_force)
     # Simple translational invariance
     for force in forces:
         force -= drift_force / forces.shape[0]
     set_of_forces.append(forces)
 
 # Phonopy post-process
-# 1st arg. is a relative lattice to the input unit cell.
-# 2nd arg. is bunch of the calculated forces.
-phonon.set_post_process([[1,0,0],[0,1,0],[0,0,1]], set_of_forces)
-print "\nPhonon frequencies at Gamma:"
-for i, freq in enumerate(phonon.get_frequencies((0,0,0))):
-    print "%3d: %10.5f Hz" %  (i+1, freq * 15.633) # THz
+phonon.produce_force_constants(forces=set_of_forces)
+print
+print "[Phonopy] Phonon frequencies at Gamma:"
+for i, freq in enumerate(phonon.get_frequencies((0, 0, 0))):
+    print "[Phonopy] %3d: %10.5f THz" %  (i + 1, freq) # THz
 
 
