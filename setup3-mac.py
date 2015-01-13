1fdeee5dcf9377600ae662c84b9385e441ac48c7
Atsushi Togo <atz.togo@gmail.com>
2015-01-13 13:02:22 +0900

    Version 0.9.7

--- setup3-mac.py
+++ setup3-mac.py
@@ -43,7 +43,7 @@ extension = Extension(
              'c/spglib/tetrahedron_method.c'])
 
 setup(name='phono3py',
-      version='0.9.6',
+      version='0.9.7',
       description='This is the phono3py module.',
       author='Atsushi Togo',
       author_email='atz.togo@gmail.com',
