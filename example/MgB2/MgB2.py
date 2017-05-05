ded361d0cda36f358ab1191ddc619b4071b42023
Atsushi Togo <atz.togo@gmail.com>
2017-05-05 15:17:21 +0900

    Update examples to adopt python 3.x. The change is mainly to make print compatible.

--- example/MgB2/MgB2.py
+++ example/MgB2/MgB2.py
@@ -9,7 +9,7 @@ cell = read_vasp("POSCAR")
 phonon = Phonopy(cell, np.diag([3, 3, 2]))
 
 symmetry = phonon.get_symmetry()
-print "Space group:", symmetry.get_international_table()
+print("Space group: %s" % symmetry.get_international_table())
 
 force_sets = parse_FORCE_SETS()
 phonon.set_displacement_dataset(force_sets)
@@ -21,5 +21,5 @@ ct = phonon.get_irreps()
 band_indices = ct.get_band_indices()
 characters = np.rint(ct.get_characters()).real
 for bi, cts in zip(band_indices, characters):
-    print np.array(bi) + 1, cts
+    print("%s %s" % (np.array(bi) + 1, cts))
 # phonon.show_character_table()
