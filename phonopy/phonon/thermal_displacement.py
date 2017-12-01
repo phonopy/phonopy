cd682bf222623c097fdc2519a03e60f3ec87aa44
J. George <JaGeo@users.noreply.github.com>
2017-12-01 10:36:44 +0100

    Correction 4
    Python3 compatibility

--- phonopy/phonon/thermal_displacement.py
+++ phonopy/phonon/thermal_displacement.py
@@ -47,12 +47,11 @@ class ThermalMotion(object):
             self._cutoff_frequency = 0
         else:
             self._cutoff_frequency = cutoff_frequency
-	  
+
         if max_frequency is not None:
-	    self._max_frequency = max_frequency
+            self._max_frequency = max_frequency
         else:
-            self._max_frequency = np.inf
-		
+                self._max_frequency = np.inf        
         self._masses = masses * AMU
         self._masses3 = np.array([[m] * 3 for m in masses]).ravel() * AMU
         self._temperatures = None
