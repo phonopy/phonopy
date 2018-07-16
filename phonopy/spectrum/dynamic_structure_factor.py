32609a6a142b92b020d17c0786491d85532622fe
Atsushi Togo <atz.togo@gmail.com>
2018-07-16 18:27:52 +0900

    Update dynamical structure factor

--- phonopy/spectrum/dynamic_structure_factor.py
+++ phonopy/spectrum/dynamic_structure_factor.py
@@ -36,14 +36,43 @@ import numpy as np
 from phonopy.units import THzToEv, Kb
 
 
+# D. Waasmaier and A. Kirfel, Acta Cryst. A51, 416 (1995)
+# f(Q) = \sum_i a_i \exp((-b_i Q^2) + c
+# Q is in angstron^-1
+# a1, b1, a2, b2, a3, b3, a4, b4, a5, b5, c
+#
+# Examples:
+#  {'Na': [3.148690, 2.594987, 4.073989, 6.046925,
+#          0.767888, 0.070139, 0.995612, 14.1226457,
+#          0.968249, 0.217037, 0.045300],  # 1+
+#   'Cl': [1.061802, 0.144727, 7.139886, 1.171795,
+#          6.524271, 19.467656, 2.355626, 60.320301,
+#          35.829404, 0.000436, -34.916604],  # 1-
+#   'Si': [5.275329, 2.631338, 3.191038, 33.730728,
+#          1.511514, 0.081119, 1.356849, 86.288640,
+#          2.519114, 1.170087, 0.145073]}  # neutral
+
 def atomic_form_factor(Q, f_x):
-    v = f_x[10]
-    for a, b in np.array(f_x[:10]).reshape(-1, 2):
-        v += a * np.exp(-b * Q ** 2)
-    return v
+    a, b = np.array(f_x[:10]).reshape(-1, 2).T
+    return (a * np.exp(-b * Q ** 2)).sum() + f_x[10]
 
 
 class DynamicStructureFactor(object):
+    """Class to generate irreducible grid points on uniform mesh grids
+
+    Attributes
+    ----------
+    qpoints: ndarray
+       q-points in reduced coordinates of reciprocal lattice with G shifted.
+       dtype='double'
+       shape=(qpoints, 3)
+    S: ndarray
+       Dynamic structure factor
+       dtype='double'
+       shape=(qpoints, phonon bands)
+
+    """
+
     def __init__(self,
                  phonon,
                  q_points,
@@ -63,12 +92,16 @@ class DynamicStructureFactor(object):
 
         self._freqs = None
         self._eigvecs = None
-        self._set_qpoints()
+        self._set_phonon()
+
+        self.qpoints = None
+        self.S = None
 
     def run(self):
-        Q_points = self._q_points + self._G
+        self.qpoints = self._q_points + self._G
+        self.S = []
         num_atom = self._primitive.get_number_of_atoms()
-        for Q, freqs, eigvecs in zip(Q_points, self._freqs, self._eigvecs):
+        for Q, freqs, eigvecs in zip(self.qpoints, self._freqs, self._eigvecs):
             temps, disps = self._get_thermal_displacements(Q)
             DW = np.exp(-0.5 * np.linalg.norm(Q) * disps[0])
             S = np.zeros(num_atom * 3, dtype='double')
@@ -78,10 +111,10 @@ class DynamicStructureFactor(object):
                                                       eigvecs[:, i])
                     n = 1.0 / (np.exp(freqs[i] * THzToEv / (Kb * self._T)) - 1)
                     S[i] = abs(F) ** 2 * (n + 1)
-            print(("%f %f %f  " + (" %f" * num_atom * 3)) %
-                  (tuple(Q) + tuple(S)))
+            self.S.append(S)
+        self.S = np.array(self.S, dtype='double', order='C')
 
-    def _set_qpoints(self):
+    def _set_phonon(self):
         self._phonon.set_qpoints_phonon(self._q_points, is_eigenvectors=True)
         self._freqs, self._eigvecs = self._phonon.get_qpoints_phonon()
 
