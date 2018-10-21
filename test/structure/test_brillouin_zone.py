0d5e7942ed4eb6bdc91377aa00b7ce1fe3b5649f
Atsushi Togo <atz.togo@gmail.com>
2018-10-21 18:47:08 +0900

    Minor performance tuning of BrillouinZone class using numpy methods and add a test with hexagonal lattice

--- test/structure/test_brillouin_zone.py
+++ test/structure/test_brillouin_zone.py
@@ -12,17 +12,26 @@ class TestBrillouinZone(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testBrillouinZone(self):
-        cell = ([[0, 2.73, 2.73], [2.73, 0, 2.73], [2.73, 2.73, 0]],
-                [[0.75, 0.75, 0.75], [0.5, 0.5, 0.5]],
-                [1, 1])
+    def test_FCC(self):
+        direct_lat = [[0, 2.73, 2.73], [2.73, 0, 2.73], [2.73, 2.73, 0]]
         mesh = [4, 4, 4]
         is_shift = [0, 0, 0]
+        self._testBrillouinZone(direct_lat, mesh, is_shift)
+
+    def test_Hexagonal(self):
+        direct_lat = [[3.0751691007292523, 0, 0],
+                      [-1.5375845503646262, 2.6631745621644800, 0],
+                      [0, 0, 3.5270080068586522]]
+        mesh = [4, 4, 4]
+        is_shift = [0, 0, 0]
+        self._testBrillouinZone(direct_lat, mesh, is_shift)
+
+    def _testBrillouinZone(self, direct_lat, mesh, is_shift):
         _, grid_address = get_stabilized_reciprocal_mesh(
             mesh,
             rotations=[np.eye(3, dtype='intc'), ],
             is_shift=is_shift)
-        rec_lat = np.linalg.inv(cell[0])
+        rec_lat = np.linalg.inv(direct_lat)
         bz_grid_address, bz_map = relocate_BZ_grid_address(
             grid_address,
             mesh,
@@ -32,7 +41,7 @@ class TestBrillouinZone(unittest.TestCase):
         qpoints = (grid_address + np.array(is_shift) / 2.0) / mesh
         bz = BrillouinZone(rec_lat)
         bz.run(qpoints)
-        sv_all = bz.get_shortest_qpoints()  # including BZ boundary duplicates
+        sv_all = bz.shortest_qpoints  # including BZ boundary duplicates
         sv = [v[0] for v in sv_all]
         bz_qpoints = (bz_grid_address + np.array(is_shift) / 2.0) / mesh
         d2_this = (np.dot(sv, rec_lat.T) ** 2).sum(axis=1)
