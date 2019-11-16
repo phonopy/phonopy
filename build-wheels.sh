80589c2d527b6f93b853e113b8ea916aefe82d3c
Atsushi Togo <atz.togo@gmail.com>
2019-11-16 16:33:44 +0900

    Update travis and pypi deploy settings

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,55 +1,10 @@
 #!/bin/bash
 set -e -x
 
-# Install a system package required by our library
-# yum install -y numpy
-yum install -y hdf5 freetype
+yum install -y hdf5 freetype-devel
 
-# ls /opt/python
-
-# # Compile wheels
-# for PYBIN in /opt/python/cp27*/bin; do
-#     # "${PYBIN}/pip" install --no-binary numpy
-#     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-# done
-
-# for PYBIN in /opt/python/cp35*/bin; do
-#     # "${PYBIN}/pip" install --no-binary numpy
-#     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-# done
-
-# for PYBIN in /opt/python/cp36*/bin; do
-#     # "${PYBIN}/pip" install --no-binary numpy
-#     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-# done
-
-# # Bundle external shared libraries into the wheels
-# ls wheelhouse/*.whl
-# for whl in wheelhouse/spglib*.whl; do
-#     auditwheel repair "$whl" -w /io/wheelhouse/
-# done
-
-# cp wheelhouse/numpy*whl /io/wheelhouse/
-
-# # Install packages and test
-# for PYBIN in /opt/python/cp27*/bin/; do
-#     "${PYBIN}/pip" install spglib --no-index -f /io/wheelhouse
-# done
-
-# for PYBIN in /opt/python/cp35*/bin/; do
-#     "${PYBIN}/pip" install spglib --no-index -f /io/wheelhouse
-# done
-
-# for PYBIN in /opt/python/cp36*/bin/; do
-#     "${PYBIN}/pip" install spglib --no-index -f /io/wheelhouse
-# done
-
-# Install a system package required by our library
-# yum install -y atlas-devel
-
-# Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    if [[ ! $PYBIN == *"34"* ]] && [[ ! $PYBIN == *"27"* ]]; then
+    if [[ $PYBIN == *"27"* ]] || [[ $PYBIN == *"35"* ]] || [[ $PYBIN == *"36"* ]] || [[ $PYBIN == *"37"* ]]; then
         "${PYBIN}/pip" install -r /io/dev-requirements.txt
         "${PYBIN}/pip" wheel /io/ -w wheelhouse/
     fi
@@ -62,7 +17,7 @@ done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    if  [[ ! $PYBIN == *"34"* ]] && [[ ! $PYBIN == *"27"* ]]; then
+    if [[ $PYBIN == *"27"* ]] || [[ $PYBIN == *"35"* ]] || [[ $PYBIN == *"36"* ]] || [[ $PYBIN == *"37"* ]]; then
         "${PYBIN}/pip" install phonopy --no-index -f /io/wheelhouse
         (cd "$HOME"; "${PYBIN}/nosetests" phonopy)
     fi
