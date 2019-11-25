6e6e16eea2e1191810d37f35c7aaf6a99b022970
Atsushi Togo <atz.togo@gmail.com>
2019-11-25 17:02:34 +0900

    Update NaCl examples following the latest API

--- example/NaCl/NaCl-read_write_fc.py
+++ example/NaCl/NaCl-read_write_fc.py
@@ -1,21 +1,20 @@
-from phonopy import Phonopy
-from phonopy.interface.vasp import read_vasp
-from phonopy.file_IO import parse_FORCE_SETS
+import phonopy
 from phonopy.file_IO import parse_FORCE_CONSTANTS, write_FORCE_CONSTANTS
 
-cell = read_vasp("POSCAR")
-phonon = Phonopy(cell,
-                 [[2, 0, 0],
-                  [0, 2, 0],
-                  [0, 0, 2]])
-force_sets = parse_FORCE_SETS()
-phonon.set_displacement_dataset(force_sets)
-phonon.produce_force_constants()
+phonon = phonopy.load(supercell_matrix=[[2, 0, 0],
+                                        [0, 2, 0],
+                                        [0, 0, 2]],
+                      primitive_matrix=[[0, 0.5, 0.5],
+                                        [0.5, 0, 0.5],
+                                        [0.5, 0.5, 0]],
+                      unitcell_filename="POSCAR",
+                      force_sets_filename="FORCE_SETS",
+                      born_filename="BORN")
 write_FORCE_CONSTANTS(phonon.get_force_constants(),
                       filename="FORCE_CONSTANTS")
 
 force_constants = parse_FORCE_CONSTANTS()
-phonon.set_force_constants(force_constants)
-phonon.symmetrize_force_constants(iteration=1)
+phonon.force_constants = force_constants
+phonon.symmetrize_force_constants()
 write_FORCE_CONSTANTS(phonon.get_force_constants(),
                       filename="FORCE_CONSTANTS_NEW")
