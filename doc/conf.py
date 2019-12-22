b9e228c7d9a758bbd1c5fef0b1259627e23e2ef0
Atsushi Togo <atz.togo@gmail.com>
2019-12-22 21:04:17 +0900

    Update documentation for v2.4.2

--- doc/conf.py
+++ doc/conf.py
@@ -166,7 +166,7 @@ html_theme_options = {
     # Options are nothing with "" (default) or the name of a valid theme
     # such as "amelia" or "cosmo".
     # 'bootswatch_theme': "united",
-    'bootswatch_theme': "Cerulean",
+    'bootswatch_theme': "cerulean",
 
     # Choose Bootstrap version.
     # Values: "3" (default) or "2" (in quotes)
