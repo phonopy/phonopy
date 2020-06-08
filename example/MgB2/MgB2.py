4b01113ec5a7bc10b327b7e0fe7a8aa93e267c26
Atsushi Togo <atz.togo@gmail.com>
2020-06-08 21:16:56 +0900

    Update examples of Al2O3, CaTiO3, Cr, and MgB2

--- example/MgB2/MgB2.py
+++ example/MgB2/MgB2.py
@@ -1,23 +1,13 @@
-from phonopy import Phonopy
-from phonopy.interface.vasp import read_vasp
-from phonopy.file_IO import parse_FORCE_SETS, parse_BORN
+import phonopy
 import numpy as np
 
-cell = read_vasp("POSCAR")
-
-# Initialize phonon. Supercell matrix has to have the shape of (3, 3)
-phonon = Phonopy(cell, np.diag([3, 3, 2]))
-
-symmetry = phonon.get_symmetry()
-print("Space group: %s" % symmetry.get_international_table())
-
-force_sets = parse_FORCE_SETS()
-phonon.set_displacement_dataset(force_sets)
-phonon.produce_force_constants()
+phonon = phonopy.load(unitcell_filename="POSCAR-unitcell",
+                      supercell_matrix=[3, 3, 2])
+print("Space group: %s" % phonon.symmetry.get_international_table())
 
 # Character table
 phonon.set_irreps([1./3, 1./3, 0], 1e-4)
-ct = phonon.get_irreps() 
+ct = phonon.get_irreps()
 band_indices = ct.get_band_indices()
 characters = np.rint(ct.get_characters()).real
 for bi, cts in zip(band_indices, characters):
