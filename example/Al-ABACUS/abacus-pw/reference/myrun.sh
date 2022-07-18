a9dc6b7fb6179ed9cbaaeead10f8a6119c5f0839
jiyuang <jiyuyang@mail.ustc.com>
2022-07-18 16:25:41 +0800

    modify examples of ABACUS

--- example/Al-ABACUS/abacus-pw/reference/myrun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-phonopy setting.conf --abacus -d
-echo 'stru_file ./STRU-001' >> INPUT
-OMP_NUM_THREADS=1  mpirun -n 8 abacus > log
-phonopy -f OUT.ABACUS/running_scf.log
-phonopy band.conf --abacus
