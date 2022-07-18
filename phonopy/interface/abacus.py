fb7b318eebcd1a8eb1a0dcb54e5605b717e5265c
jiyuang <jiyuyang@mail.ustc.com>
2022-07-18 20:58:10 +0800

    modify docstring

--- phonopy/interface/abacus.py
+++ phonopy/interface/abacus.py
@@ -267,8 +267,7 @@ def _skip_notes(line):
 
 
 def _list_elem2strip(a, ds=string.whitespace):
-    """Strip element of list with `str` type"""
-
+    """Strip element of list with `str` type."""
     def list_strip(s):
         return s.strip(ds)
 
