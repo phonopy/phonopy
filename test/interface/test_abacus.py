754e5dd757c38781bdc96f8a00e96658241e6f24
jiyuang <jiyuyang@mail.ustc.com>
2022-07-18 20:36:52 +0800

    rename test file

--- test/interface/test_abacus.py
+++ test/interface/test_abacus.py
@@ -12,7 +12,7 @@ data_dir = os.path.dirname(os.path.abspath(__file__))
 def test_read_abacus():
     """Test of read_ABACUS."""
     cell, pps, orbitals = read_abacus(os.path.join(data_dir, "NaCl-abacus.stru"), elements=['Na', 'Cl'])
-    filename = os.path.join(data_dir, "NaCl-abinit-pwscf-abacus.yaml")
+    filename = os.path.join(data_dir, "NaCl-abinit-pwscf.yaml")
     cell_ref = read_cell_yaml(filename)
     assert (np.abs(cell.cell - cell_ref.cell) < 1e-5).all()
     diff_pos = cell.scaled_positions - cell_ref.scaled_positions
