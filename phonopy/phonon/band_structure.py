Fix calculation of q_direction for band struture with NAC
* Before modification, the NAC is applied to Gamma point only.

Before commit c2b1002 (v2.21.2), the q_direction is given as a argument of the _solve_dm_on_path function, after the commit c2b1002 (v2.21.2), because no q_direction can be applied, NAC is applied to Gamma point only.

phonopy/phonon/band_structure.py
@@ -712,9 +712,7 @@ class BandStructure:
             distances_on_path.append(self._distance)
 
             if isinstance(self._dynamical_matrix, DynamicalMatrixNAC):
-                q_direction = None
-                if (np.abs(q) < 0.0001).all():  # For Gamma point
-                    q_direction = path[0] - path[-1]
+                q_direction = path[0] - path[-1]
                 self._dynamical_matrix.run(q, q_direction=q_direction)
             else:
                 self._dynamical_matrix.run(q)