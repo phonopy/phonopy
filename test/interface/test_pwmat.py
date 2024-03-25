91fd1e32d7df5a5aa2245e45f4b47e306c2256a3
Liang Jianxin <2509426238@qq.com>
2024-03-25 10:48:12 +0800

    add test file for pwmat interface

---
+++ test/interface/test_pwmat.py
@@ -0,0 +1,28 @@
+"""Tests for ABACUS calculater interface."""
+
+import os
+
+import numpy as np
+
+from phonopy.interface.pwmat import read_atom_config 
+from phonopy.interface.phonopy_yaml import read_cell_yaml
+from phonopy.structure import cells
+
+data_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def test_read_pwmat():
+    """Test of read_PWmat."""
+    cell = read_atom_config(os.path.join(data_dir, "Si-pwmat.config"))
+    filename = os.path.join(data_dir, "Si-pwmat.yaml")
+    cell_ref = read_cell_yaml(filename)
+    # assert (np.abs(cell.cell - cell_ref.cell) < 1e-5).all()
+    assert cells.isclose(cell, cell_ref)
+    diff_pos = cell.scaled_positions - cell_ref.scaled_positions
+    diff_pos -= np.rint(diff_pos)
+    assert (np.abs(diff_pos) < 1e-5).all()
+    for s, s_r in zip(cell.symbols, cell_ref.symbols):
+        assert s == s_r
+
+if __name__ == "__main__":
+    test_read_pwmat()
