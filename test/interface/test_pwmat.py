c341cd62198c9a27d0071f8d5275643a6553065e
Liang Jianxin <2509426238@qq.com>
2024-03-25 15:30:17 +0800

    change the test file for interface of pwmat

--- test/interface/test_pwmat.py
+++ test/interface/test_pwmat.py
@@ -1,11 +1,11 @@
-"""Tests for ABACUS calculater interface."""
+"""Tests for PWmat calculater interface."""
 
 import os
 
 import numpy as np
 
-from phonopy.interface.pwmat import read_atom_config 
 from phonopy.interface.phonopy_yaml import read_cell_yaml
+from phonopy.interface.pwmat import read_atom_config
 from phonopy.structure import cells
 
 data_dir = os.path.dirname(os.path.abspath(__file__))
@@ -16,13 +16,9 @@ def test_read_pwmat():
     cell = read_atom_config(os.path.join(data_dir, "Si-pwmat.config"))
     filename = os.path.join(data_dir, "Si-pwmat.yaml")
     cell_ref = read_cell_yaml(filename)
-    # assert (np.abs(cell.cell - cell_ref.cell) < 1e-5).all()
     assert cells.isclose(cell, cell_ref)
     diff_pos = cell.scaled_positions - cell_ref.scaled_positions
     diff_pos -= np.rint(diff_pos)
     assert (np.abs(diff_pos) < 1e-5).all()
     for s, s_r in zip(cell.symbols, cell_ref.symbols):
         assert s == s_r
-
-if __name__ == "__main__":
-    test_read_pwmat()
