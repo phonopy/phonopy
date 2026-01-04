ff28ab3c5232d033bae2c1bc5d29b22bedcbc2c1
Atsushi Togo <atz.togo@gmail.com>
2026-01-04 21:14:19 +0900

    Add test for --fz option

--- phonopy/cui/phonopy_argparse.py
+++ phonopy/cui/phonopy_argparse.py
@@ -951,6 +951,7 @@ class PhonopyMockArgs:
     cell_filename: str | os.PathLike | None = None
     conf_filename: str | os.PathLike | None = None
     create_force_sets: list[str | os.PathLike] | None = None
+    create_force_sets_zero: list[str | os.PathLike] | None = None
     fc_symmetry: bool | None = None
     fc_spg_symmetry: bool | None = None
     filename: Sequence[os.PathLike | str] | None = None
